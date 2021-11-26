7c2af092e2b0081d957b6b672c47a757bf2f4bec
bifooh <pereirasamueljesus@gmail.com>
2021-11-25 21:18:14 -0500

    first well made

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,13 +54,44 @@ function scrollTop(){
 window.addEventListener('scroll', scrollTop)
 
 /*===== MIXITUP FILTER PORTFOLIO =====*/ 
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
 
 /* Link active portfolio */ 
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
 
+function activePortfolio(){
+    if (linkPortfolio) {
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
 /*===== SWIPER CAROUSEL =====*/ 
+const swiper = new Swiper('.testimonial__container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  });
 
 /*===== GSAP ANIMATION =====*/ 
-
+gsap.from('.home__img', {opacity: 0, duration:2, delay:.5,  x:60})
+gsap.from('.home__data', {opacity: 0, duration:2, delay:.8,  y:25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay:1,  y:25, ease:'expo.out', stagger:.2})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay:1.5,  y:25, ease:'expo.out', stagger:.2})
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay:1.8,  y:25, ease:'expo.out', stagger:.2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay:2.3,  y:25, ease:'expo.out', stagger:.2})
 
